e5b8cb0987ac08181e9f8f2986a5f373600c00e8
Rene Steinmaurer <55176260+ReneSteinmaurer@users.noreply.github.com>
2025-04-25 22:23:00 +0200

    add: own service for websocket and manage it with rxjs

--- frontend/src/app/core/landing-page/katalogisiere-medium-dialog/katalogisiere-medium.service.ts
+++ frontend/src/app/core/landing-page/katalogisiere-medium-dialog/katalogisiere-medium.service.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RestResponse} from '../../../shared/types/rest-response';
 import {LandingPageService} from '../landing-page.service';
+import {WebSocketMessage, WebsocketService} from '../../../shared/services/websocket.service';
 
 export type KatalogisiereCommand = {
   MediumId: string
@@ -10,42 +11,21 @@ export type KatalogisiereCommand = {
   Standort: string
 }
 
-const WebSocketMessageTypeKeys = [
-  'PROJECTION_UPDATED'
-] as const
-
-type WebSocketMessageType = typeof WebSocketMessageTypeKeys[number]
-
-export type WebSocketMessage<T> = {
-  type: WebSocketMessageType
-  data: T
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class KatalogisiereMediumService {
   private http = inject(HttpClient)
-  private socket?: WebSocket
+  private websocketService = inject(WebsocketService)
   landingPageService = inject(LandingPageService)
 
   katalogisiereMedium(cmd: KatalogisiereCommand) {
     this.http.post<RestResponse<string>>('http://localhost:8080/bibliothek/katalogisiere-medium', {...cmd}).subscribe(res => {
-      console.log(res.data)
-      this.openWebsocketConnection(res.data)
+      this.websocketService.listen(res.data).subscribe((msg) => {
+          if (msg.type === 'PROJECTION_UPDATED') {
+            this.landingPageService.getAllMedien()
+          }
+      });
     });
-  }
-
-  openWebsocketConnection(aggregateId: string) {
-    console.log('INIT WEBSOCKET CONNECTION')
-    this.socket = new WebSocket(`ws://localhost:8080/ws?aggregateId=${aggregateId}`)
-
-    this.socket.onmessage = (event) => {
-      const message = JSON.parse(event.data) as WebSocketMessage<string>
-      console.log(message)
-      if (message.type === 'PROJECTION_UPDATED') {
-        this.landingPageService.getAllMedien()
-      }
-    }
   }
 }
--- frontend/src/app/shared/services/websocket.service.ts
+++ frontend/src/app/shared/services/websocket.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
+import {Observable} from 'rxjs';
+
+const WebSocketMessageTypeKeys = [
+  'PROJECTION_UPDATED'
+] as const
+
+type WebSocketMessageType = typeof WebSocketMessageTypeKeys[number]
+
+export type WebSocketMessage<T> = {
+  type: WebSocketMessageType
+  data: T
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
+  private wsSubject: WebSocketSubject<WebSocketMessage<string>> | null = null;
+  private socket?: WebSocket
 
-  openNewConnection() {}
+  listen(aggregateId: string): Observable<WebSocketMessage<string>>{
+    this.wsSubject = webSocket<WebSocketMessage<string>>({
+      url: `ws://localhost:8080/ws?aggregateId=${aggregateId}`,
+      deserializer: msg => JSON.parse(msg.data)
+    })
+    return this.wsSubject.asObservable();
+  }
+
 }
